7ee708ba4a60f51818e6a06ff19157c2f5f3e0a2
Jarmo Sokka <jsokka@hotmail.com>
2025-10-29 20:29:46 +0200

    Implement 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,6 +5,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const points = [
+    good * 1,
+    neutral * 0,
+    bad * -1
+  ]
+
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : points.reduce((a, b) => a + b, 0) / total
+  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -15,6 +25,7 @@ const App = () => {
       </div>
       <h2>Statistics</h2>
       <div>Good: {good} | Neutral: {neutral} | Bad: {bad}</div>
+      <div>Total: {total} | Average: {average} | Positive: {positivePercentage} %</div>
     </div>
   )
 }
