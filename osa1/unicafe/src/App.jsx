82157f6e43376d97f1373ec1397bc2c409e0ea4e
Jarmo Sokka <jsokka@hotmail.com>
2025-10-29 20:37:42 +0200

    Implement 1.8 and 1.9

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,6 +5,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <div>
+        <Button onClick={() => setGood(good + 1)} text="good 😋" />
+        <Button onClick={() => setNeutral(neutral + 1)} text="neutral 🙂" />
+        <Button onClick={() => setBad(bad + 1)} text="bad 🤮" />
+      </div>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
   const points = [
     good * 1,
     neutral * 0,
@@ -15,25 +36,18 @@ const App = () => {
   const average = total === 0 ? 0 : points.reduce((a, b) => a + b, 0) / total
   const positivePercentage = total === 0 ? 0 : (good / total) * 100
 
+  if (total === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
   return (
     <div>
-      <h1>Give feedback</h1>
-      <div>
-        <Button onClick={() => setGood(good + 1)} text="good 😋" />
-        <Button onClick={() => setNeutral(neutral + 1)} text="neutral 🙂" />
-        <Button onClick={() => setBad(bad + 1)} text="bad 🤮" />
-      </div>
-      <h2>Statistics</h2>
       <div>Good: {good} | Neutral: {neutral} | Bad: {bad}</div>
       <div>Total: {total} | Average: {average} | Positive: {positivePercentage} %</div>
     </div>
   )
 }
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
 
 export default App
