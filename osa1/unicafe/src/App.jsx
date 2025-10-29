9700b6ba9a4355cc9e9872531513abc1920c8fcf
Jarmo Sokka <55681011+jsokka@users.noreply.github.com>
2025-10-29 21:50:35 +0200

    Osa 1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <div>
+        <Button onClick={() => setGood(good + 1)} text="good 😋" />
+        <Button onClick={() => setNeutral(neutral + 1)} text="neutral 🙂" />
+        <Button onClick={() => setBad(bad + 1)} text="bad 🤮" />
+      </div>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const points = [
+    good * 1,
+    neutral * 0,
+    bad * -1
+  ]
+
+  const total = good + neutral + bad
+  const average = (total === 0 ? 0 : points.reduce((a, b) => a + b, 0) / total).toFixed(1)
+  const positivePercentage = (total === 0 ? 0 : (good / total) * 100).toFixed(1)
+
+  if (total === 0) {
+    return (
+      <div>No feedback given yet</div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <tr><StatisticsLine text="good" value={good} /></tr>
+        <tr><StatisticsLine text="neutral" value={neutral} /></tr>
+        <tr><StatisticsLine text="bad" value={bad} /></tr>
+        <tr><StatisticsLine text="all" value={total} /></tr>
+        <tr><StatisticsLine text="average" value={average} /></tr>
+        <tr><StatisticsLine text="positive" value={`${positivePercentage} %`} /></tr>
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticsLine = ({ text, value }) => (
+  <>
+    <td>{text}</td>
+    <td>{value}</td>
+  </>
+)
+
+export default App
