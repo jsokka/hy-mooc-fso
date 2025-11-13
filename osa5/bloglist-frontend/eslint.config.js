83ec55c2ca0516bb1975d10d209a68e1f3d477d2
Jarmo Sokka <jsokka@hotmail.com>
2025-11-13 21:11:07 +0200

    Fix eslint config

--- osa5/bloglist-frontend/eslint.config.js
+++ osa5/bloglist-frontend/eslint.config.js
@@ -36,7 +36,7 @@ export default [
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
       'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'on'
+      'no-console': 'off'
     }
   }
 ]
